f47a595bbd08846713d9460425490402e052ae09
axel-achart <axel.achart@gmail.com>
2025-02-06 19:20:59 +0100

    job 07 finish, day OK

--- Jour04/job07.py
+++ Jour04/job07.py
@@ -1,89 +1,108 @@
-# Job 07 : Jeu du Blackjack
+# Job 07 : Blackjack
 
 import random
 
-
+# Classe Carte
 class Carte:
-    def __init__(self):
-        self.valeurs = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Reine", "Roi", "AS"]
-        self.couleurs = ["Coeur", "Carreaux", "Pique", "Trèfle"]
-        self.valeur = random.choice(self.valeurs)
-        self.couleur = random.choice(self.couleurs)
-
-    def afficherCarte(self):
-        print(f"{self.valeur} de {self.couleur} ({self.pointsParCartes()} points)")
-
-    def pointsParCartes(self):
-        if self.valeur == "Valet" or self.valeur == "Reine" or self.valeur == "Roi":
-            self.points = 10
-            return self.points
+    # Constructeur Initialisation
+    def __init__(self, valeur, couleur):
+        self.valeur = valeur
+        self.couleur = couleur
+    
+    # Afficher la carte
+    def afficher(self):
+        print(f"{self.valeur} de {self.couleur}")
+    
+    # Points de la carte
+    def points(self):
+        if self.valeur in ["Valet", "Reine", "Roi"]:
+            return 10
         elif self.valeur == "AS":
-            self.points = int(input("Voulez-vous prendre 1 ou 11 points ? : "))
-            return self.points
-        elif self.valeur == "2":
-            self.points = 2
-            return self.points
-        elif self.valeur == "3":
-            self.points = 3
-            return self.points
-        elif self.valeur == "4":
-            self.points = 4
-            return self.points
-        elif self.valeur == "5":
-            self.points = 5
-            return self.points
-        elif self.valeur == "6":
-            self.points = 6
-            return self.points
-        elif self.valeur == "7":
-            self.points = 7
-            return self.points
-        elif self.valeur == "8":
-            self.points = 8
-            return self.points
-        elif self.valeur == "9":
-            self.points = 9
-            return self.points
-        elif self.valeur == "10":
-            self.points = 10
-            return self.points
-        
+            return 11  # Géré dans le calcul du score pour ajuster
+        else:
+            # Point de la carte en fonction de son numéro
+            return int(self.valeur)
 
+# Classe Jeu
 class Jeu:
+    # Constructeur Initialisation
     def __init__(self):
-        self.paquet = []    # Avec 52 cartes
+        valeurs = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Reine", "Roi", "AS"]
+        couleurs = ["Coeur", "Carreaux", "Pique", "Trèfle"]
+        # Création du paquet de cartes
+        self.paquet = [Carte(valeur, couleur) for valeur in valeurs for couleur in couleurs]
+        # Mélanger le paquet
+        random.shuffle(self.paquet)
+        # Initialisation des mains
+        self.joueur_main = []
+        self.croupier_main = []
+    
+    # Distribuer les cartes et supprimer du paquet la carte prise
+    def distribuer_cartes(self):
+        for _ in range(2):
+            self.joueur_main.append(self.paquet.pop())
+            self.croupier_main.append(self.paquet.pop())
+    
+    # Afficher la main
+    def afficher_main(self, main, joueur):
+        print(f"Main de {joueur}:")
+        for carte in main:
+            carte.afficher()
+        print(f"Score: {self.calculer_score(main)}\n")
+    
+    # Calculer le score
+    def calculer_score(self, main):
+        score = sum(carte.points() for carte in main)
+        as_count = sum(1 for carte in main if carte.valeur == "AS")
+        
+        while score > 21 and as_count:
+            score -= 10  # L'As passe de 11 à 1
+            as_count -= 1
+        
+        return score
+    
+    # Tour du joueur
+    def tour_joueur(self):
+        while True:
+            self.afficher_main(self.joueur_main, "Joueur")
+            if self.calculer_score(self.joueur_main) > 21:
+                print("Vous avez dépassé 21, vous perdez !")
+                return False
+            choix = input("Voulez-vous une autre carte ? (o/n) : ").lower()
+            if choix == 'o':
+                self.joueur_main.append(self.paquet.pop())
+            else:
+                break
+        return True
+    
+    # Tour du croupier
+    def tour_croupier(self):
+        while self.calculer_score(self.croupier_main) < 17:
+            self.croupier_main.append(self.paquet.pop())
+        self.afficher_main(self.croupier_main, "Croupier")
+    
+    # Déterminer le gagnant
+    def determiner_gagnant(self):
+        joueur_score = self.calculer_score(self.joueur_main)
+        croupier_score = self.calculer_score(self.croupier_main)
+        
+        if joueur_score > 21:
+            print("Le croupier gagne !")
+        elif croupier_score > 21 or joueur_score > croupier_score:
+            print("Vous gagnez !")
+        elif joueur_score < croupier_score:
+            print("Le croupier gagne !")
+        else:
+            print("Égalité !")
+    
+    # Jouer
+    def jouer(self):
+        print("\nDébut du Blackjack !")
+        self.distribuer_cartes()
+        if self.tour_joueur():
+            self.tour_croupier()
+            self.determiner_gagnant()
 
-    # Méthode Lancement du jeu
-    def debut(self):
-        print("\nDébut du jeu")
-
-    # Méthode pour recevoir les 2 cartes
-    def accesseurs(self):
-        pass
-
-    # Méthode affichage des cartes attribuées
-    def afficherCartesAttribués(self):
-        pass
-
-    # Méthode pour choisir entre 'prendre' ou 'passer'
-    def choisir(self):
-        pass
-
-    # Méthode le croupier prend des cartes jusqu'à au moins 17pts
-    def boucleCroupier(self):
-        pass
-
-    # Méthode pour retourner le score de chacun
-    def scores(self):
-        pass
-
-    # Méthode pour vérifier qui a gagné
-    def verifGagner(self):
-        pass
-
-
-premiere_carte = Carte()
-deuxieme_carte = Carte()
-
-premiere_carte.afficherCarte()
-deuxieme_carte.afficherCarte()
+# Lancer une partie
+jeu = Jeu()
+jeu.jouer()
